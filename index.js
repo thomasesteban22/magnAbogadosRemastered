7d6dfebc097faec7b46735fff3da1dd4ed41b4d8
Thomas Esteban Ariza Forero <thomas.esteban@gmail.com>
2022-10-18 14:16:20 -0500

    Arreglo del path de index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser')
-const routes = require('./routes/index.js');
+const routes = require('./src/routes');
 
 //setings
 app.set('port', 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 //middlewares
@@ -14,11 +14,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //routes
-app.use(require('./routes/index'));
+app.use(require('./src/routes'));
 
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 //server-on
 app.listen(app.get('port'), () =>{
